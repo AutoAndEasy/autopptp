2a108e5b051742995839b08cf6c73dd461080a42
Chier Xuefei <ktaog6@126.com>
2013-02-27 23:00:42 +0800

    format the script

--- rhel6/autopptp-1.0.0-1.el6.x86_64.sh
+++ rhel6/autopptp-1.0.0-1.el6.x86_64.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-##
-##This is use for PPTP VPN
-##Author:翅儿学飞
+################ Script Info ################		
 
-#############Set Base Var#############
+## Program: This is use for PPTP VPN
+## Author:chier xuefei
+## Date: 2013-02-26
+## Update:None
+
+
+################ Env Define ################
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:~/sbin
+LANG=C
+export PATH
+export LANG
+
+################ Var Setting ################
 
 MyIP="1.2.3.4"
 NatNet="192.168.194"
@@ -13,13 +24,50 @@ BasePkg="wget gcc ppp policycoreutils iptables"
 SrcHost="https://raw.github.com"
 SrcPath="/AutoAndEasy/autopptp/master/rhel6/"
 
-##############    Main    ##############
+################ Func Define ################ 
+function _info_msg() {
+_header
+echo -e " |                                                                |"
+echo -e " |               Thank you for use autopptp script!               |"
+echo -e " |                                                                |"
+echo -e " |                         Version: 1.0.0                         |"
+echo -e " |                                                                |"
+echo -e " |                     http://www.idcsrv.com                      |"
+echo -e " |                                                                |"
+echo -e " |                   Author:翅儿学飞(chier xuefei)                |"
+echo -e " |                      Email:myregs@126.com                      |"
+echo -e " |                         QQ:1810836851                          |"
+echo -e " |                         QQ群:61749648                          |"
+echo -e " |                                                                |"
+echo -e " |          Hit [ENTER] to continue or ctrl+c to exit             |"
+echo -e " |                                                                |"
+printf " o----------------------------------------------------------------o\n"	
+ read entcs 
+clear
+}
+
+function _header() {
+	printf " o----------------------------------------------------------------o\n"
+	printf " | :: AutoPPTP                                v1.0.0 (2013/02/26) |\n"
+	printf " o----------------------------------------------------------------o\n"	
+}
+
+##Program Function
+
+################ Main ################
+clear
+_info_msg
+
+if [ `id -u` != "0" ]; then
+echo -e "You need to be be the root user to run this script.\nWe also suggest you use a direct root login, not su -, sudo etc..."
+exit 1
+fi
 
 if [ ! -d $HomeDir ]; then
         mkdir -p $HomeDir
 fi
 
-cd $HomeDir
+cd $HomeDir || exit 1
 
 
 ## Install software
